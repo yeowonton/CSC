a54b0e6a864c981b0dc3ccdeb1bfecd3bdaf6edf
Ivan Zimmerman <izimmerman@berkeley.edu>
2024-11-28 20:49:21 -0800

    background better color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				mainBackground: {
+					light: "#91f9e5",
+					dark: "#499167",
+				}
+			}
+		},
 	},
 	plugins: [],
 }
